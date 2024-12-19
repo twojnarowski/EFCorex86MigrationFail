4d30bef2b36256f280d8f49a890820ed34b0b1cf
tymonomyt <tymoteusz1138@gmail.com>
2024-12-19 14:54:53 +0100

    Add project files.

---
+++ EFCorex86MigrationFail/Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCorex86MigrationFail
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=aspnet-MvcMovie;Integrated Security=SSPI;AttachDBFilename=|DataDirectory|\\Movies.mdf");
+            }
+        }
+    }
+}
---
+++ EFCorex86MigrationFail/Form1.cs
@@ -0,0 +1,10 @@
+namespace EFCorex86MigrationFail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
